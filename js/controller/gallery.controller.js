7bad3c860ce77d06d2c24f09c3288aa22a8cf3ab
Lioz Fired <friedlioz@gmail.com>
2025-04-25 11:53:47 +0300

    Change the gallery render from html to js

--- js/controller/gallery.controller.js
+++ js/controller/gallery.controller.js
@@ -1,5 +1,17 @@
 'use strict'
 
+function onInit() {
+    renderGallery()
+}
+
+function renderGallery() {
+    const elGallery = document.querySelector('.gallery')
+    const srtHtml = gImgs.map(img => (
+        `<img src="${img.url}" id="${img.id}" onclick="onImgSelect(this)"></img>`
+    ))
+    elGallery.innerHTML = srtHtml.join('')
+}
+
 function onImgSelect(elImg) {
     setImg(+elImg.id)
     defineCanvas()
