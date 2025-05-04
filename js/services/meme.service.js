e90293effa859e6a46ce9062fb57ccd1819049f8
Lioz Fired <friedlioz@gmail.com>
2025-05-04 15:33:26 +0300

    Implemenet MQ

--- js/services/meme.service.js
+++ js/services/meme.service.js
@@ -3,6 +3,22 @@
 var gImgs = [
     { id: 1, url: 'img/1.jpg', keywords: ['trump', 'irreverent'] },
     { id: 2, url: 'img/2.jpg', keywords: ['dog', 'kiss'] },
+    { id: 3, url: 'img/3.jpg', keywords: ['dog', 'sleep', 'baby'] },
+    { id: 4, url: 'img/4.jpg', keywords: ['cat', 'sleep'] },
+    { id: 5, url: 'img/5.jpg', keywords: ['win', 'baby', 'trick'] },
+    { id: 6, url: 'img/6.jpg', keywords: ['explain', 'irreverent'] },
+    { id: 7, url: 'img/7.jpg', keywords: ['baby', 'surprised'] },
+    { id: 8, url: 'img/8.jpg', keywords: ['charm', 'irreverent'] },
+    { id: 9, url: 'img/9.jpg', keywords: ['baby', 'trick', 'laugh'] },
+    { id: 10, url: 'img/10.jpg', keywords: ['obama', 'laugh', 'trick'] },
+    { id: 11, url: 'img/11.jpg', keywords: ['kiss', 'wrestling'] },
+    { id: 12, url: 'img/12.jpg', keywords: ['justice', 'win'] },
+    { id: 13, url: 'img/13.jpg', keywords: ['win', 'irreverent', 'cheers'] },
+    { id: 14, url: 'img/14.jpg', keywords: ['win', 'trick', 'shock'] },
+    { id: 15, url: 'img/15.jpg', keywords: ['exactly', 'explain'] },
+    { id: 16, url: 'img/16.jpg', keywords: ['laugh', 'trick'] },
+    { id: 17, url: 'img/17.jpg', keywords: ['putin', 'irreverent', 'win'] },
+    { id: 18, url: 'img/18.jpg', keywords: ['exactly', 'explain', 'charm'] },
 ]
 
 var gMeme = {
